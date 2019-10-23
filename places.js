82aa3f1555e36cd23231993cb37ab240ce4e794d
XingMeansOK <771267736@qq.com>
2019-10-23 14:51:03 +0800

    Update places.js

--- places.js
+++ places.js
@@ -41,6 +41,16 @@ window.onload = () => {
             str.push('是否经过偏移：' + (data.isConverted ? '是' : '否'));
             positionIndicator.innerHTML += str.join('<br>');
             
+            debugger
+            const qiu = document.createElement('a-sphere');
+            const latitude = data.position.lat;
+            const longitude = data.position.lng;
+            qiu.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+            qiu.setAttribute('radius', '250');
+            qiu.setAttribute('color', "#EF2D5E");
+            scene.appendChild(qiu);
+            
+            
             AMap.service(["AMap.PlaceSearch"], function() {
                 //构造地点查询类
                 var placeSearch = new AMap.PlaceSearch({ 
@@ -61,38 +71,45 @@ window.onload = () => {
                             const icon = document.createElement('a-image');
                             icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
                             icon.setAttribute('name', place.name);
-                            icon.setAttribute('src', './map-marker.png');
+                            icon.setAttribute('src', 'https://raw.githubusercontent.com/XingMeansOK/GeoAR.js/master/examples/assets/map-marker.png');
                             
                             // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
                             icon.setAttribute('scale', '20, 20');
 
                             icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
-                            const clickListener = function(ev) {
-                                ev.stopPropagation();
-                                ev.preventDefault();
+//                             const clickListener = function(ev) {
+//                                 ev.stopPropagation();
+//                                 ev.preventDefault();
 
-                                const name = ev.target.getAttribute('name');
+//                                 const name = ev.target.getAttribute('name');
 
-                                const el = ev.detail.intersection && ev.detail.intersection.object.el;
+//                                 const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-                                if (el && el === ev.target) {
-                                    const label = document.createElement('span');
-                                    const container = document.createElement('div');
-                                    container.setAttribute('id', 'place-label');
-                                    label.innerText = name;
-                                    container.appendChild(label);
-                                    document.body.appendChild(container);
+//                                 if (el && el === ev.target) {
+//                                     const label = document.createElement('span');
+//                                     const container = document.createElement('div');
+//                                     container.setAttribute('id', 'place-label');
+//                                     label.innerText = name;
+//                                     container.appendChild(label);
+//                                     document.body.appendChild(container);
 
-                                    setTimeout(() => {
-                                        container.parentElement.removeChild(container);
-                                    }, 1500);
-                                }
-                            };
+//                                     setTimeout(() => {
+//                                         container.parentElement.removeChild(container);
+//                                     }, 1500);
+//                                 }
+//                             };
 
-                            icon.addEventListener('click', clickListener);
+//                             icon.addEventListener('click', clickListener);
 
-                            scene.appendChild(icon);
+                            // scene.appendChild(icon);
+                            
+                            const qiu = document.createElement('a-sphere');
+                            qiu.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+                            qiu.setAttribute('name', place.name);
+                            qiu.setAttribute('radius', '250');
+                            qiu.setAttribute('color', "#EF2D5E");
+                            scene.appendChild(qiu);
                         })
                     }else{
 
