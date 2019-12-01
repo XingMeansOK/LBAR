8800e35c844b42aa04e66e73796b4442e4d41e56
XingMeansOK <771267736@qq.com>
2019-12-01 17:39:07 +0800

    Create store.js

---
+++ store.js
@@ -0,0 +1,70 @@
+// 全局变量
+const store = {
+    yaw: 0, // 移动设备的航空次序欧拉角
+    pitch: 0,
+    roll: 0,
+    scene: null, // three场景
+    camera: null, // three相机
+    renderer: new THREE.WebGLRenderer({alpha: true }), // trree渲染器
+    videoTack: null, // 后置摄像头视频流
+    eventName: getDeviceOrientationEventName(), // 设备姿态变换事件名称
+    constraints: { // 视频流限制条件
+        audio: false, 
+        video: { 
+            facingMode: { exact: "environment" },
+            // aspectRatio:1440/3120,
+            // aspectRatio: window.innerHeight/window.innerWidth, // 高！！宽！！比
+            // width: { ideal: 3120 },
+            // height: { ideal: 1440 },
+
+            width: { ideal: 800 },
+            height: { ideal: 400 },
+        } 
+    }
+}
+
+// isSafari();
+
+
+/**
+ * Get device orientation event name, depends on browser implementation.
+ * @returns {string} event name
+ */
+function getDeviceOrientationEventName () {
+    if ('ondeviceorientationabsolute' in window) {
+        var eventName = 'deviceorientationabsolute'
+    } 
+    else if ('ondeviceorientation' in window) {
+        var eventName = 'deviceorientation'
+    } else {
+        var eventName = ''
+        console.error('Compass not supported')
+    }
+
+    return eventName
+}
+
+function isSafari(){
+        // if Safari
+    if (!!navigator.userAgent.match(/Version\/[\d.]+.*Safari/)) {
+        // iOS 13+
+        if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+            var handler = function() {
+                console.log('Requesting device orientation permissions...')
+                DeviceOrientationEvent.requestPermission();
+                document.removeEventListener('touchend', handler);
+            };
+
+            document.addEventListener('touchend', function() { handler() }, false);
+
+            alert('After camera permission prompt, please tap the screen to active geolocation.');
+        } else {
+            var timeout = setTimeout(function () {
+                alert('Please enable device orientation in Settings > Safari > Motion & Orientation Access.')
+            }, 750);
+            window.addEventListener(eventName, function () {
+                clearTimeout(timeout);
+            });
+        }
+    }
+}
