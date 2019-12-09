f41edec6f2edb9948375f3eda667209b1fddf844
XingMeansOK <771267736@qq.com>
2019-12-10 00:07:00 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,18 +2,19 @@ function test_1() {
     var loader = new THREE.GLTFLoader();
     
     loader.load( './Flamingo.glb', function ( gltf ) {
+
         var mesh = gltf.scene.children[ 0 ];
-        var s = 0.35;
+        var s = 0.035;
         mesh.scale.set( s, s, s );
-        mesh.position.y = 15;
-        mesh.position.x = 15;
+        mesh.position.x = 45;
         mesh.rotation.y = - 1;
         mesh.castShadow = true;
         mesh.receiveShadow = true;
         store.scene.add( mesh );
         var mixer = new THREE.AnimationMixer( mesh );
         mixer.clipAction( gltf.animations[ 0 ] ).setDuration( 1 ).play();
-        mixers.push( mixer );
+        store.mixers.push( mixer );
+        
     } );
 }
 
